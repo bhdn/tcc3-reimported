3bac6be241a4a447eaed2cf99b7941ae282bc0f4
Bogdano Arendartchuk <debogdano@gmail.com>
2011-04-14 00:05:43 -0300

    Added parameter show= to allow redirecting stdout or not

--- src/tcc3/util.py
+++ src/tcc3/util.py
@@ -4,11 +4,20 @@ from tcc3 import Error
 class CommandError(Error):
     pass
 
-def system_command(args):
-    proc = subprocess.Popen(args, shell=False)
+def system_command(args, show=False):
+    if show:
+        stdout = subprocess.STDOUT
+        stderr = None
+    else:
+        stdout = subprocess.PIPE
+        stderr = subprocess.STDOUT
+    proc = subprocess.Popen(args, shell=False, stdout=stdout,
+            stderr=stderr, bufsize=1024 * 1024 * 5)
     proc.wait()
+    output = ""
+    if show:
+        output = proc.stdout.read()
     if proc.returncode != 0:
         cmdline = subprocess.list2cmdline(args)
-        output = proc.stdout.read()
         raise CommandError, "command failed: %s:\n%s" % (cmdline, output)
-    return output, returncode
+    return output, proc.returncode
