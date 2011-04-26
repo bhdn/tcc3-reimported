be9f0d4fc0c8d865adca3daae65e8fc97a5d714d
Bogdano Arendartchuk <debogdano@gmail.com>
2011-04-26 11:50:36 -0300

    a few more values to test

--- src/tcc3/defaults.py
+++ src/tcc3/defaults.py
@@ -7,18 +7,20 @@ collector-type = vmstat
 main-database-name = main
 trained-database-name = trained
 databases-dir = ./databases
-learning-method = svm
+
+learning-method = svm-multiclass
 
 knn-number-neighbours = 10
 
-future-values = 10
-svm-window-size = 20
+future-values = 6
+svm-window-size = 30
 
-t = 2
-C = 2.0
-gamma = 0.5
+t = 0
+C = 0.02
+gamma = 0.00005
+degree = 5
 
-params = -t %(t)s -c %(C)s -g %(gamma)s
+params = -t %(t)s -c %(C)s -g %(gamma)s -d %(degree)s -v 5
 
 libsvm-learn-command = ../../libsvm/libsvm-3.1/svm-train %(params)s
 libsvm-classify-command = ../../libsvm/libsvm-3.1/svm-predict
@@ -26,12 +28,12 @@ libsvm-classify-command = ../../libsvm/libsvm-3.1/svm-predict
 svmlight-learn-command = ../../libsvm/svm_learn %(params)s
 svmlight-classify-command = ../../libsvm/svm_classify
 
-svm-samples = 8000
-svm-test = 4000
+svm-samples = 10000
+svm-test = 8000
 svm-test-dir = ./databases/svmlight-test/
 svm-trained-dir = ./databases/svmlight-trained/
 
-cpu-usage-ranges = 2
+cpu-usage-ranges = 3
 max-cpu-value = 100
 
 [conf]
