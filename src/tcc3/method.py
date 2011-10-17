9070b1b6ea7164a149eb4694bfc3d5f92164c13e
Bogdano Arendartchuk <debogdano@gmail.com>
2011-10-17 18:35:48 -0200

    Not finished yet

--- src/tcc3/method.py
+++ src/tcc3/method.py
@@ -218,7 +218,7 @@ class AutoRegressiveMethod(Method, WindowGeneratorMixIn):
         self._dump_coefs(coefs, machine)
 
     def predict(self, machine, window):
-        pass
+        raise NotImplementedError
 
 class SVMBaseMethod(Method, WindowGeneratorMixIn):
 
