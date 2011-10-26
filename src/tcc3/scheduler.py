a91878b38dc7545f07f6a76ae30b1489bb0e44c1
Bogdano Arendartchuk <debogdano@gmail.com>
2011-10-26 12:18:31 -0200

    Implemented the “best-fit increasing” scheduler

--- src/tcc3/scheduler.py
+++ src/tcc3/scheduler.py
@@ -1,5 +1,7 @@
 import time
 import collections
+import threading
+import Queue
 import logging
 
 class Scheduler(object):
@@ -15,16 +17,34 @@ class Scheduler(object):
         self.windowsize = int(config.window_size)
         self.nranges = int(config.cpu_usage_ranges)
         self.logger = logging.getLogger("tcc3.scheduler")
+        self._statsource = Queue.Queue()
+        self._finished = threading.Event()
 
-    def start(self):
+    def _collect_stats(self):
+        try:
+            last = 0
+            while not self._finished.is_set():
+                self.logger.debug("collector tick")
+                stats = list(self.vmm.collect_stats())
+                self._statsource.put(stats)
+                diff = time.time() - last
+                waittime = max(self.interval - diff, 0)
+                self._finished.wait(waittime)
+                last = time.time()
+        finally:
+            self._finished.set()
+
+    def _schedule(self):
         readings = {} # {guestname: deque of stats}
         canpredict = False
-        while True:
-            self.logger.debug("scheduler tick %r", readings)
+        while not self._finished.is_set():
             predictions = {} # guestname: predicted class_
             hostpredictions = {}
-            hostsinfo = set(self.vmm.collect_stats())
             # collect information about hosts and predict cpu usage for
+            try:
+                hostsinfo = self._statsource.get(timeout=1)
+            except Queue.Empty:
+                continue
             # guests:
             for hostinfo in hostsinfo:
                 for guestinfo in hostinfo.guests:
@@ -42,7 +62,6 @@ class Scheduler(object):
                                 + class_
                         readings[guestinfo.name].popleft()
                         canpredict = True
-            time.sleep(1)
             if canpredict:
                 self.logger.debug("predictions: %r", predictions)
                 self.logger.debug("hostpredictions: %r", hostpredictions)
@@ -50,67 +69,61 @@ class Scheduler(object):
                 # used as in "class_"):
                 maxcapacity = dict((hi.name, hi.maxcpus * (self.nranges - 1))
                                     for hi in hostsinfo)
-                availcapacity = dict((hi.name,
-                    (maxcapacity[hi.name] - hostpredictions[hi.name]))
-                    for hi in hostsinfo)
+                availcapacity = {}
+                availcapacity.update(maxcapacity)
+                availmemory = dict((hi.name, hi.freememory)
+                                    for hi in hostsinfo)
+                for hi in hostsinfo:
+                    for guest in hi.guests:
+                        availmemory[hi.name] += guest.memoryused
+                self.logger.debug("maxcapacity: %r", maxcapacity)
+                self.logger.debug("availcapacity: %r", availcapacity)
                 # used capacity as pointed by the predictions:
                 # compute a list of the hosts that are most likely to
                 # accomodate idle vms (those that already host many vms and are
                 # able to keep even more, thus resulting in less vm-migrations
                 # and more chance of being able to put idle hosts in
                 # power-saving mode)
-                sortcriteria = lambda hi: (len(hi.guests),
-                        maxcapacity[hi.name], hi.freememory)
-                tophosts = sorted(hostsinfo, key=sortcriteria, reverse=True)
-                bottomhosts = list(reversed(tophosts)) # hosts that likely
-                                                       # to be shut down
+                guestmap = {}
+                for hi in hostsinfo:
+                    for guest in hi.guests:
+                        guestmap[guest] = hi
+                # host name and guest names are used as sorting criteria to
+                # ensure there will be no bouncing between a set of
+                # machines that are identical (ie. one machine randomly
+                # being consired first for the packing algorithm)
+                hostscriteria = lambda hi: (maxcapacity[hi.name],
+                        availmemory[hi.name], hi.name)
+                tophosts = sorted(hostsinfo, key=hostscriteria, reverse=True)
+                guestscriteria = lambda g: (predictions[g.name], g.memoryused, g.name)
+                sortedguests = sorted(guestmap.keys(), key=guestscriteria)
+                self.logger.debug("hosts: %r", tophosts)
+                self.logger.debug("guests: %r", sortedguests)
+                self.logger.debug("guestmap: %r", guestmap)
                 migrations = [] # [(srchost, guest, dsthost), ...]
-                migrated = set()
-                weakhosts = []
-                availmemory = dict((hi.name, hi.freememory)
-                        for hi in hostsinfo)
-                # first 'force' of migrations: try to put guests toghether
-                # as most as possible by pulling those idle in hosts:
-                for i, tophost in enumerate(tophosts):
-                    if tophost in weakhosts:
-                        # no need to go further, all hosts from now on are
-                        # 'weak'
-                        break
-                    for j, bottomhost in enumerate(reversed(tophosts)):
-                        if tophost == bottomhost:
-                            continue
-                        for guest in bottomhost.guests:
-                            if guest in migrated:
-                                continue
-                            if (availmemory[tophost.name] > guest.memoryused
-                                    and availcapacity[tophost.name] > predictions[guest.name]):
-                                migrations.append((bottomhost, guest, tophost))
-                                availmemory[tophost.name] -= guest.memoryused
-                                availcapacity[tophost.name] -= predictions[guest.name]
-                                availmemory[bottomhost.name] += guest.memoryused
-                                availcapacity[bottomhost.name] += predictions[guest.name]
-                                migrated.add(guest)
-                                weakhosts.append(bottomhost)
-                # second force: spread guests that are demanding more
-                # resources from (overloaded) hosts and still trying to
-                # preserve 'bottomhosts' from not going into power-save
-                # mode
-                sortcriteria = lambda guest: (predictions[guest.name],
-                        guest.memoryused)
-                for hostinfo in hostsinfo:
-                    if availcapacity[hostinfo.name] < 0:
-                        topguests = sorted(hostinfo.guests,
-                                key=sortcriteria, reverse=True)
-                        for guest in topguests:
-                            # reversed(weakhosts) means 'those that were not considered
-                            # the most unlikely to receive migrations'
-                            for weakhost in reversed(weakhosts):
-                                if (availmemory[weakhost.name] > guest.memoryused
-                                        and availcapacity[weakhost.name] > predictions[guest.name]):
-                                    migrations.append((hostinfo, guest, weakhost))
-                                    availmemory[weakhost.name] -= guest.memoryused
-                                    availcapacity[weakhost.name] -= predictions[guest.name]
-                                    migrated.add(guest)
-                self.logger.debug("migrations: %r", migrations)
-                time.sleep(1)
+                for host in tophosts:
+                    for guest in sortedguests[:]:
+                        guesthost = guestmap[guest]
+                        if (availcapacity[host.name] > predictions[guest.name]
+                                and availmemory[host.name] > guest.memoryused):
+                            # good to migrate:
+                            if guesthost != host:
+                                migrations.append((guesthost, guest, host))
+                                availcapacity[guesthost.name] += predictions[guest.name]
+                                availmemory[guesthost.name] += guest.memoryused
+                            availcapacity[host.name] -= predictions[guest.name]
+                            availmemory[host.name] -= guest.memoryused
+                            sortedguests.remove(guest)
+                self.logger.info("migrations: %r", migrations)
+                for srchost, guest, dsthost in migrations:
+                    self.logger.info("migrating %s from %s to %s", guest.name,
+                            srchost.name, dsthost.name)
+                    self.vmm.migrate(srchost.name, guest.name, dsthost.name)
 
+    def start(self):
+        try:
+            t = threading.Thread(target=self._collect_stats)
+            t.start()
+            self._schedule()
+        finally:
+            self._finished.set()
