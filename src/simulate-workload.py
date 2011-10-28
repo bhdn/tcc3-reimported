2120de62780055b5913c078b6e22766b34997064
Bogdano Arendartchuk <debogdano@gmail.com>
2011-10-28 21:55:45 -0200

    Some improvements

--- src/simulate-workload.py
+++ src/simulate-workload.py
@@ -9,23 +9,27 @@ def handler(signum, frame):
     global running
     running = False
 
-def work_for(usage=0.50, interval=10)
+def work_for(usage=0.50, interval=10):
     global running
     signal.signal(signal.SIGALRM, handler)
     cur = time.time()
     dur = interval * usage
-    print "sleeping for", dur
-    signal.setitimer(signal.ITIMER_REAL, dur)
-    running = True
-    while running:
-        x = 0.001 + 323232.0
+    print "working for", dur
+    if dur > 0.0:
+        signal.setitimer(signal.ITIMER_REAL, dur)
+        running = True
+        while running:
+            x = 0.001 + 323232.0
     print "awake!"
     now = time.time()
     time.sleep(max(interval - (now - cur), 0))
 
 interval = float(sys.argv[1])
 
-for rawvalue in open(sys.argv[2]):
-    value = float(rawvalue.strip())
+for line in open(sys.argv[2]):
+    rawvalue = line.strip()
+    if not rawvalue:
+        continue
+    value = float(rawvalue)
     print "current load:", value
-    work_for(value, interval)
+    work_for(value / 100.0, interval)
