2c297b8cded995010d6ff9cc56b280b407d27989
Bogdano Arendartchuk <debogdano@gmail.com>
2011-11-07 10:27:23 -0200

    A script to parse the tcc-scheduler output to generate stats

---
+++ src/parse-sched-log.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import re
+import sys
+
+# DEBUG: guestmap: {<guest 'seggie'>: <host 'host0'>, <guest 'n4'>: <host 'host0'>, <guest 'n2'>: <host 'host0'>, <guest 'n6'>: <host 'host0'>, <guest 'n3'>: <host 'host0'>}
+
+is_re = re.compile("DEBUG: (?P<guest>[^ ]+) is (?P<perc>[^%]+)%")
+pair_re = re.compile("<guest '(?P<guest>[^']+)'>: <host '(?P<host>[^']+)'>")
+
+first = None
+all = []
+cur = {}
+for line in open(sys.argv[1]):
+    match = is_re.search(line)
+    if match:
+        guest = match.group("guest")
+        perc = float(match.group("perc"))
+        if first is None:
+            first = guest
+        elif guest == first:
+            all.append(cur)
+            cur = {}
+        cur[guest] = perc
+    found = pair_re.findall(line)
+    if found:
+        usage = {}
+        for guest, host in found:
+            if guest in cur:
+                if host in usage:
+                    usage[host] += cur[guest]
+                else:
+                    usage[host] = cur[guest]
+        for host, perc in usage.iteritems():
+            print host, perc
