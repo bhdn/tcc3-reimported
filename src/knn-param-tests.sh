aa2a0cf323e3fc0d421c41ddd7b02c9c90f790bd
Bogdano Arendartchuk <debogdano@gmail.com>
2011-11-06 23:49:19 -0200

    Smaller version to fit in the text

--- src/knn-param-tests.sh
+++ src/knn-param-tests.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -ex
 for N in {2..15} 30 40 50 75 100 150; do
-	echo "***** N: $N"
-	rm -rf databases/
-	for h in n2 n3 n4 n6 seggie; do
-		./tcc3-collect -o "tcc3.knn-number-neighbours=$N" \
-			-H $h vmstat-$h-partial.log
-	done 
-	for h in n2 n3 n4 n6 seggie; do
-		./tcc3-train -o "tcc3.knn-number-neighbours=$N" \
-			-H $h -v &
-	done 
-	wait
-	for h in n2 n3 n4 n6 seggie; do
-		./tcc3-test -o "tcc3.knn-number-neighbours=$N" \
-			-H $h &
-	done 
-	wait
+  echo "***** N: $N"
+  rm -rf databases/
+  for h in n2 n3 n4 n6 seggie; do
+    ./tcc3-collect -o "tcc3.knn-number-neighbours=$N" \
+      -H $h vmstat-$h-partial.log
+  done 
+  for h in n2 n3 n4 n6 seggie; do
+    ./tcc3-train -o "tcc3.knn-number-neighbours=$N" \
+      -H $h -v &
+  done 
+  wait
+  for h in n2 n3 n4 n6 seggie; do
+    ./tcc3-test -o "tcc3.knn-number-neighbours=$N" \
+      -H $h &
+  done 
+  wait
 done
