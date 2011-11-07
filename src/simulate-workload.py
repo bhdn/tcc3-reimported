415c74d33696d63f81e27b9ac07a3ed482f6ea70
Bogdano Arendartchuk <debogdano@gmail.com>
2011-11-07 03:19:28 -0200

    Make it smaller to fit in documentation

--- src/simulate-workload.py
+++ src/simulate-workload.py
@@ -4,37 +4,32 @@ import time
 import signal
 
 running = False
-
 counter = 0
 
 def handler(signum, frame):
-    global running
-    running = False
+  global running
+  running = False
 
 def work_for(usage=0.50, interval=10):
-    global running
-    global counter
-    signal.signal(signal.SIGALRM, handler)
-    cur = time.time()
-    dur = interval * usage
-    print "working for", dur
-    if dur > 0.0:
-        signal.setitimer(signal.ITIMER_REAL, dur)
-        running = True
-        while running:
-            x = 0.001 + 323232.0
-            counter += 1
-    print "awake, counter:", counter
-    now = time.time()
-    time.sleep(max(interval - (now - cur), 0))
+  global running
+  global counter
+  signal.signal(signal.SIGALRM, handler)
+  cur = time.time()
+  dur = interval * usage
+  if dur > 0.0:
+    signal.setitimer(signal.ITIMER_REAL, dur)
+    running = True
+    while running:
+      x = 0.001 + 323232.0
+      counter += 1
+  now = time.time()
+  time.sleep(max(interval - (now - cur), 0))
 
 interval = float(sys.argv[1])
-
 for line in open(sys.argv[2]):
-    rawvalue = line.strip()
-    if not rawvalue:
-        continue
-    value = float(rawvalue)
-    print "current load:", value
-    work_for(value / 100.0, interval)
-    sys.stdout.flush()
+  rawvalue = line.strip()
+  if not rawvalue:
+    continue
+  value = float(rawvalue)
+  work_for(value / 100.0, interval)
+  sys.stdout.flush()
