2e52d82154ad68e8fa020df46256fc290c65d083
Bogdano Arendartchuk <debogdano@gmail.com>
2011-11-06 23:52:19 -0200

    Smaller version to fit in the text

--- src/svm-param-test-lviv-finer.sh
+++ src/svm-param-test-lviv-finer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 for C in 0.03125 0.0625 0.125 0.25 0.5 0.75 1 1.25 1.5 2; do
-  for gamma in 0.03125 0.0625 0.125 0.25 0.5 0.75 1 1.25 1.5 1.75 2; do
+  for gamma in 0.03125 0.0625 0.125 0.25 0.5 0.75 1 1.25 1.5 \
+	  1.75 2; do
     echo "***** C: $C, gamma: $gamma"
     rm -rf databases/
     for h in n2 n3 n4 n6 seggie; do
