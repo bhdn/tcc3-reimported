2cbe350a886f4d28741170cd612569d976b47d2a
Bogdano Arendartchuk <debogdano@gmail.com>
2011-11-06 23:49:03 -0200

    Smaller version to fit in the text

--- src/svm-param-test-lviv-finer.sh
+++ src/svm-param-test-lviv-finer.sh
@@ -1,26 +1,23 @@
 #!/bin/bash -ex
-
 for C in 0.03125 0.0625 0.125 0.25 0.5 0.75 1 1.25 1.5 2; do
-	for gamma in 0.03125 0.0625 0.125 0.25 0.5 0.75 1 1.25 1.5 1.75 2; do
-		echo "***** C: $C, futurevalue: $gamma"
-
-		rm -rf databases/
-
-		for h in n2 n3 n4 n6 seggie; do
-			./tcc3-collect -o "tcc3.svm-params=-s 0 -t 2 -c $C -g $gamma" \
-				-H $h vmstat-$h-partial.log
-		done 
-		wait
-		for h in n2 n3 n4 n6 seggie; do
-			./tcc3-train -o "tcc3.svm-params=-s 0 -t 2 -c $C -g $gamma" \
-				-H $h -v &
-		done 
-		wait
-		for h in n2 n3 n4 n6 seggie; do
-			echo "testing host $h"
-			./tcc3-test -o "tcc3.svm-params=-s 0 -t 2 -c $C -g $gamma" \
-				-H $h &
-		done 
-		wait
-	done
+  for gamma in 0.03125 0.0625 0.125 0.25 0.5 0.75 1 1.25 1.5 1.75 2; do
+    echo "***** C: $C, gamma: $gamma"
+    rm -rf databases/
+    for h in n2 n3 n4 n6 seggie; do
+      ./tcc3-collect -o "tcc3.svm-params=-s 0 -t 2 -c $C -g $gamma" \
+        -H $h vmstat-$h-partial.log
+    done 
+    wait
+    for h in n2 n3 n4 n6 seggie; do
+      ./tcc3-train -o "tcc3.svm-params=-s 0 -t 2 -c $C -g $gamma" \
+        -H $h -v &
+    done 
+    wait
+    for h in n2 n3 n4 n6 seggie; do
+      echo "testing host $h"
+      ./tcc3-test -o "tcc3.svm-params=-s 0 -t 2 -c $C -g $gamma" \
+        -H $h &
+    done 
+    wait
+  done
 done
